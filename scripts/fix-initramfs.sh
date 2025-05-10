977f3a4839aea2c54c9c390a1d192849f44af11b
uhs-robert <robert.hill@uphillsolutions.tech>
2025-05-10 12:56:42 -0400

    Add initramfs fix
    This fix is a script that should trigger when dnf post transaction for a
    kernel update occurs.
    The script is called by
    /etc/dnf/pluggins.post-transaction-actions.d/initramfs-rebuild.conf

---
+++ scripts/fix-initramfs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+latest_kernel=$(rpm -q --last kernel-core | head -n1 | awk '{print $1}' | cut -d'-' -f3-)
+echo "Fixing initramfs for $latest_kernel"
+
+sudo akmods --kernels "$latest_kernel"
+sudo dracut --force "/boot/initramfs-${latest_kernel}.img" "$latest_kernel"
