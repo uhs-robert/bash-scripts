b6877b7f9700528235535f00bba36475cbc416f2
uhs-robert <robert.hill@uphillsolutions.tech>
2025-06-09 12:45:05 -0400

    Fix kernels to kernel for akmods --param

--- scripts/fix-initramfs.sh
+++ scripts/fix-initramfs.sh
@@ -3,5 +3,5 @@
 latest_kernel=$(rpm -q --last kernel-core | head -n1 | awk '{print $1}' | cut -d'-' -f3-)
 echo "Fixing initramfs for $latest_kernel"
 
-sudo akmods --kernels "$latest_kernel"
+sudo akmods --kernel "$latest_kernel"
 sudo dracut --force "/boot/initramfs-${latest_kernel}.img" "$latest_kernel"
